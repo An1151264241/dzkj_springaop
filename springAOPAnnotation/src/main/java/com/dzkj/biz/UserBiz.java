d65fa36a273895f20ca9fdee287faf73266f266c
11512 <11512@DESKTOP-NJ15N7J>
2019-08-10 16:25:29 +0800

    第一次传递到服务器

---
+++ springAOPAnnotation/src/main/java/com/dzkj/biz/UserBiz.java
@@ -0,0 +1,18 @@
+package com.dzkj.biz;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserBiz {
+	
+	public void findAll() {
+		System.out.println("查询所有用户");
+	}
+	
+	
+	public boolean delete(int id) {
+		System.out.println(1/0);
+		System.out.println("删除用户成功,id:"+id);
+		return true;
+	}
+}
---
+++ springAOPAnnotation/src/test/java/springAOPAnnotation/AOPTest.java
@@ -0,0 +1,13 @@
+package springAOPAnnotation;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dzkj.biz.UserBiz;
+
+public class AOPTest {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserBiz ub = ac.getBean(UserBiz.class);
+		ub.findAll();
+	}
+}
